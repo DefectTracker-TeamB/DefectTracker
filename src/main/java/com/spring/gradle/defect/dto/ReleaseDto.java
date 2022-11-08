8887e04abeb89b6812a23e9e1026e59873c8a842
Akalya97 <akalya.gnanasekaram@gmail.com>
2022-11-08 19:59:58 +0530

    Release
    Crud

--- src/main/java/com/spring/gradle/defect/dto/ReleaseDto.java
+++ src/main/java/com/spring/gradle/defect/dto/ReleaseDto.java
@@ -1,5 +1,33 @@
 package com.spring.gradle.defect.dto;
 
 public class ReleaseDto {
-
+	private int id;
+	private String releaseVersion ;
+	private String apiLink ;
+	private String uploadFile ;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getReleaseVersion() {
+		return releaseVersion;
+	}
+	public void setReleaseVersion(String releaseVersion) {
+		this.releaseVersion = releaseVersion;
+	}
+	public String getApiLink() {
+		return apiLink;
+	}
+	public void setApiLink(String apiLink) {
+		this.apiLink = apiLink;
+	}
+	public String getUploadFile() {
+		return uploadFile;
+	}
+	public void setUploadFile(String uploadFile) {
+		this.uploadFile = uploadFile;
+	}
+	
 }
--- src/main/java/com/spring/gradle/defect/util/Constants.java
+++ src/main/java/com/spring/gradle/defect/util/Constants.java
@@ -1,5 +1,9 @@
 package com.spring.gradle.defect.util;
 
 public class Constants {
-
+	public static final String CREATERELEASE="/create";
+	public static final String GETALLRELEASE="/getAll";
+	public static final String GETBYID="/getById/{id}";
+	public static final String DELETERELEASE="/delete/{id}";
+	public static final String UPDATERELEASE="/update";
 }
