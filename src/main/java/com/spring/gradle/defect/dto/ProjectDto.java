a9a6486fa5247111ef69712525032219bd84d5f7
Akalya97 <akalya.gnanasekaram@gmail.com>
2022-11-08 12:05:30 +0530

    Project
    Entity

--- src/main/java/com/spring/gradle/defect/dto/ProjectDto.java
+++ src/main/java/com/spring/gradle/defect/dto/ProjectDto.java
@@ -1,5 +1,48 @@
 package com.spring.gradle.defect.dto;
 
 public class ProjectDto {
+	private int proId;
+	private String name;
+	private String startDate;
+	private String endDate;
+	private String description;
+	private String uploadFile;
+	public int getProId() {
+		return proId;
+	}
+	public void setProId(int proId) {
+		this.proId = proId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getUploadFile() {
+		return uploadFile;
+	}
+	public void setUploadFile(String uploadFile) {
+		this.uploadFile = uploadFile;
+	}
+	
 
 }
