4333d34975a3007a7a85fd4794c4b834c2e93673
Akalya97 <akalya.gnanasekaram@gmail.com>
2022-11-10 15:56:05 +0530

    Status
    Create Crud

---
+++ src/main/java/com/spring/gradle/defect/dto/StatusDto.java
@@ -0,0 +1,28 @@
+package com.spring.gradle.defect.dto;
+
+public class StatusDto {
+
+		private Long id;
+		private String status_name;
+		private String change_date;
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getStatus_name() {
+			return status_name;
+		}
+		public void setStatus_name(String status_name) {
+			this.status_name = status_name;
+		}
+		public String getChange_date() {
+			return change_date;
+		}
+		public void setChange_date(String change_date) {
+			this.change_date = change_date;
+		}
+		
+	}
+
--- src/main/java/com/spring/gradle/defect/util/Constants.java
+++ src/main/java/com/spring/gradle/defect/util/Constants.java
@@ -2,4 +2,13 @@ package com.spring.gradle.defect.util;
 
 public class Constants {
 
+	
+	//Status
+			public static final String CREATESTATUS="/create";
+			public static final String GET_SATATUS="/get/{id}";
+			public static final String GET_ALLSATATUS="/getAll";
+			public static final String PUT_SATATUS="/update/{id}";
+			public static final String DELE_STATUS="/delete/{id}";
+	
+	
 }
